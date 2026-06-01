4451fae56b2363e08caf2adb78bee6a3fa870420
Wilson <wilson.rojas.apolinario@alumnos.uta.cl>
2026-06-01 17:04:13 -0400

    Update env.prod.ts

--- Frontend/src/Enviroments/env.prod.ts
+++ Frontend/src/Enviroments/env.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://192.168.50.21/prod/api',
+    apiUrl: 'http://192.168.50.21/ssm-adm/api',
     socketUrl: 'http://192.168.50.21',
     socketPath: '/prod/socket.io'
   };
