3e8c8598e0488841caad79cb8f095a63296f980e
Wilson <wilson.rojas.apolinario@alumnos.uta.cl>
2026-06-01 20:29:50 -0400

    Update app.routes.ts
    se añade los imports de dashboard y admin-productos

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { RegisterComponent } from './register/register';
+import { dashboardComponent } from './dashboard/dashboard';
+import { AdminProductosComponent } from './admin-productos/admin-productos';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: dashboardComponent },
+  { path: 'admin-productos', component: AdminProductosComponent }
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
